732acd9a32dca6028081946026a3d341e63d8fef
tathyadoot-png <tathyadoot@gmail.com>
2026-01-14 11:29:35 +0530

    updatea

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -68,7 +68,7 @@ const Footer = ({ lang }: { lang: Lang }) => {
             <div className="hidden lg:block h-4 w-px bg-white/10" />
             <div className="flex items-center gap-2">
               <span className="text-white/80 text-[10px] font-bold uppercase tracking-widest flex items-center gap-2">
-                Made with <Heart className="w-3 h-3 text-primary fill-primary" /> in Satna
+                Made with <Heart className="w-3 h-3 text-primary fill-primary" /> 
               </span>
             </div>
           </div>
