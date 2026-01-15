bbb17973e6a3f0716eec89079e37040d3b535f60
tathyadoot-png <tathyadoot@gmail.com>
2026-01-15 11:31:59 +0530

    fixes

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,10 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  build: {
+  outDir: "dist"
+}
+,
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
